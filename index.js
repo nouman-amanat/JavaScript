5cd17253645b603549b033f2b8069a4152a6d935
nouman-amanat <noumanamanat930@gmail.com>
2024-03-02 15:35:58 -0800

    Starting JavaScript

---
+++ index.js
@@ -0,0 +1,22 @@
+const a=10;
+const b=20;
+const c=30;
+console.log(a+b+c);
+
+
+const name="Pakistan Zindabad";
+console.log(name.length);
+
+let number=100;
+number=number + 10;
+number += 10;
+console.log(number);
+
+
+let age=20;
+let string="My age is:"+age+" years"; 
+console.log(string);
+
+const text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum sunt voluptas necessitatibus";
+count=text.length;
+console.log(count);
