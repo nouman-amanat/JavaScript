42fb1ccc25a50e52bce56b28105db94371b168b7
nouman-amanat <noumanamanat930@gmail.com>
2024-03-02 17:27:45 -0800

    in this file we will discuss about datatype and ECMA Script

---
+++ Datatypes.js
@@ -0,0 +1,20 @@
+// Data Types in JavaScript
+// use Strict Mode         treat all javascript as strict mode this is new methode in new version 
+
+const SomeText= "Hello World";
+const SomeNumber= 123;
+const SomeBoolean= true;
+const SomeArray= [1,2,3,4,5];
+const SomeObject= {name: "John", age: 30, city: "New York"};
+console 
+console.log(typeof(SomeText), typeof(SomeNumber), typeof(SomeBoolean), typeof(SomeArray), typeof(SomeObject));
+
+
+
+
+
+
+
+
+
+
