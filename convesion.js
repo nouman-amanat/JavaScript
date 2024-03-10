d1fbaf97bbd759f0916d16409648e026cc9ac6ad
nouman-amanat <noumanamanat930@gmail.com>
2024-03-09 22:50:25 -0800

    conversion of dataTypes

--- convesion.js
+++ convesion.js
@@ -1,13 +1,26 @@
 const score=100;
 console.log(typeof score);
 
+
+// convert number to string
 const valueInnumber=String(score);
 console.log(typeof valueInnumber);
-
+// this is not a purely number
 const value="123abc";
 console.log(Number(value));
 
+const  number= "1";
+console.log(Boolean(number));
 
-const  number= True;
-console.log(number);
+
+const  IsLogedIn= "Nouman";
+const booleanIsLogedIn =(Boolean(IsLogedIn));
+console.log(booleanIsLogedIn);
+
+
+const someNumber=33;
+
+console.log(typeof(someNumber));
+console.log(string(someNumber));
+
 
