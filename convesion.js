2eb214d04db6658db6ef0132b5f7de775651532a
nouman-amanat <noumanamanat930@gmail.com>
2024-03-09 22:42:53 -0800

    conversion of Datatypes

---
+++ convesion.js
@@ -0,0 +1,13 @@
+const score=100;
+console.log(typeof score);
+
+const valueInnumber=String(score);
+console.log(typeof valueInnumber);
+
+const value="123abc";
+console.log(Number(value));
+
+
+const  number= True;
+console.log(number);
+
