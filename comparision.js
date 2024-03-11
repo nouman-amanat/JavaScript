8a0c4dc392889cdbde031dc5f7f42ec318909367
nouman-amanat <noumanamanat930@gmail.com>
2024-03-11 10:47:03 -0700

    study and compare different opreator

---
+++ comparision.js
@@ -0,0 +1,22 @@
+// const age=23;
+// const b=20;
+
+// console.log(a+b); // 30
+// compare this with the following snippet and see the difference
+ console.log(2 > 1);
+ console.log(2 >= 1);
+ console.log(2 < 1);
+ console.log(2 == 1);
+ console.log(2 != 1);
+
+
+ console.log("2" > 1);
+ console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
